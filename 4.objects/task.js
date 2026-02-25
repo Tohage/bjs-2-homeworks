3f02446c85dab76dd5314e353bcdb3d08d1d414c
Tohage89 <avgorin89@gmail.com>
2026-02-25 18:29:13 +0300

    Фикс кода

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -10,22 +10,25 @@ Student.prototype.setSubject = function (subjectName) {
 };
 
 Student.prototype.addMarks = function (...marksToAdd) {
-  if (this.hasOwnProperty('marks')) {
-    this.marks.push(...marksToAdd);
+  if (!this.marks) {
+    return;
   }
+  
+  this.marks.push(...marksToAdd);
 };
 
 Student.prototype.getAverage = function () {
   if (!this.marks || this.marks.length === 0) {
     return 0;
   }
-  
-  const sum = this.marks.reduce((acc, curr) => acc + curr, 0);
+
+  const sum = this.marks.reduce((acc, mark) => acc + mark, 0);
   return sum / this.marks.length;
 };
 
 Student.prototype.exclude = function (reason) {
   delete this.subject;
   delete this.marks;
+  
   this.excluded = reason;
 };
