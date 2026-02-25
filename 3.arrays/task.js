b40264d56c671a1e64cd9fa97c99781e3f802e05
Tohage89 <avgorin89@gmail.com>
2026-02-25 18:05:54 +0300

    Реализованы задачи по массивам

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,18 @@
 function compareArrays(arr1, arr2) {
-  
+    return arr1.length === arr2.length && arr1.every((v, i) => v === arr2[i]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  let result = users
+    .filter(user => user.gender === gender)
+    .map(user => user.age)
+    .reduce((acc, item, index, array) => {
+        acc += item;
+        if (index === array.length - 1) {
+            return acc / array.length;
+        }
+        return acc;
+    }, 0);
+    
+  return result;
 }
