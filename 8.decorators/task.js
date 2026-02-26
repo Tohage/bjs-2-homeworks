6b80773e5124fe130b7d81e70497e144c5c63c92
Tohage89 <avgorin89@gmail.com>
2026-02-26 18:12:10 +0300

    Реализованы задачи по теме декораторы

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,59 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+  let cache = [];
+
+  function wrapper(...args) {
+    const hash = md5(args);
+
+    let objectInCache = cache.find((item) => item.hash === hash);
+
+    if (objectInCache) {
+      console.log("Из кеша: " + objectInCache.value);
+      return "Из кеша: " + objectInCache.value;
+    }
+
+    let result = func(...args);
+
+    cache.push({ hash, value: result });
+
+    if (cache.length > 5) {
+      cache.shift();
+    }
+
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  }
+
+  return wrapper;
 }
+
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
+  let timeoutId = null;
   
+  function wrapper(...args) {
+    wrapper.allCount++;
+
+    if (!timeoutId) {
+      func(...args); 
+      wrapper.count++; 
+    }
+
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+
+    timeoutId = setTimeout(() => {
+      func(...args); 
+      wrapper.count++; 
+      timeoutId = null; 
+    }, delay);
+  }
+
+  wrapper.count = 0;   
+  wrapper.allCount = 0; 
+
+  return wrapper;
 }
+
